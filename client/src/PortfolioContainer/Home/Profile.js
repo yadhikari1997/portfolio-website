786799427f5ecbb4b92d77114d9a7638571136e3
yadhikari <yakeenstan@gmail.com>
2023-02-16 13:45:39 -0600

    created profile details

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,44 +1,44 @@
 import React from "react";
 import Typical from "react-typical";
+import "./Profile.css";
+
 export default function Profile() {
   return (
     <div className="profile-container">
       <div className="profile-parent">
-        <div className="profile-detail">
+        <div className="profile-details">
           <div className="colz">
             <div className="colz-icon">
-                
+              <a
+                href="https://stackoverflow.com/questions/45558193/how-to-make-vs-code-change-language-to-javascript-react-when-open-a-jsx-file"
+                target="_blank"
+              >
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a
+                href="https://github.com/yadhikari1997/portfolio-website"
+                target="_blank"
+              >
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
             </div>
-            <a
-              href="https://stackoverflow.com/questions/45558193/how-to-make-vs-code-change-language-to-javascript-react-when-open-a-jsx-file"
-              target="_blank"
-            >
-              <i className="fa fa-facebook-square"></i>
-            </a>
-            <a
-              href="https://github.com/yadhikari1997/portfolio-website"
-              target="_blank"
-            >
-              <i className="fa fa-google-plus-square"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-instagram"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-youtube-square"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-twitter"></i>
-            </a>
           </div>
-        </div>
-          <div className="pofile-details-name">
+          <div className="profile-details-name">
             <span className="primary-text">
               {" "}
               HEllo, I'm <span className="gighlighted-text">Yakeen</span>
             </span>
           </div>
-          <div className="pofile-details-role">
+          <div className="profile-details-role">
             <span className="primary-text">
               {" "}
               <h1>
@@ -64,13 +64,14 @@ export default function Profile() {
                 operations.
               </span>
             </span>
-            <div className="profile-options">
-              <button className="btn primary-btn"> Hire Me </button>
-              <a href="resume.pdf" download="Yakeen resume.pdf">
-                <button className="btn highlighted-btn">Get Resume</button>
-              </a>
-            </div>
           </div>
+        <div className="profile-options">
+          <button className="btn primary-btn"> Hire Me </button>
+          <a href="resume.pdf" download="Yakeen resume.pdf">
+            <button className="btn highlighted-btn">Get Resume</button>
+          </a>
+        </div>
+        </div>
         <div className="profile-picture">
           <div className="profile-picture-background"></div>
         </div>
